b4eaff0e9ea2bb7caf74e2719af67cfbaeb6124d
AnnaZ <annazaikina271@gmail.com>
2023-03-06 20:40:47 +0300

    123

---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+string elements = Console.ReadLine();
+int[] Array = GetArrayFromString(elements);
+int count = GetCountPosNum(Array);
+Console.WriteLine($"{count}");
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int GetCountPosNum(int[] Array)
+{
+    int count = 0;
+    foreach (var item in Array)
+        if (item > 0) count++;
+    return count;
+
+}
